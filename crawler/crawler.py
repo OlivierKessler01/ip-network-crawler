8aeab13633f09a005fce2474d87a0fd583f6a275
Olivier kessler <olivier.kessler@protonmail.com>
2021-01-06 17:15:44 +0100

    Initial commit

---
+++ crawler/crawler.py
@@ -0,0 +1,17 @@
+import uuid
+
+from smbprotocol.connection import Connection, Dialects
+
+class Crawler:
+    smb_ports = []
+    rsync_ports = []
+    mysql_ports = []
+    
+    def print_test(self):
+        print("test")
+
+    def scan_for_ports_opened(protocol):
+        return 0
+    
+       
+    
---
+++ main.py
@@ -0,0 +1,9 @@
+from crawler.crawler import Crawler
+
+def main():
+    crawler = Crawler()
+    crawler.print_test()
+
+if __name__ == "__main__":
+    main()
+
