c3ca2eb35413de4446338ffcd116dcac139f5c80
Olivier kessler <olivier.kessler@protonmail.com>
2021-01-06 21:31:00 +0100

    Add timer to treatment

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,16 +1,22 @@
 import uuid
 
 from smbprotocol.connection import Connection, Dialects
+import nmap3
 
+'''
+Class that crawls the ipv4 network in research for open files
+'''
 class Crawler:
     smb_ports = []
     rsync_ports = []
     mysql_ports = []
-    
-    def print_test(self):
-        print("test")
 
-    def scan_for_ports_opened(protocol):
+    def scan_for_ports_opened(self, protocol) -> int:
+
+        nmap = nmap3.Nmap()
+        result = nmap.scan_top_ports("scanme.nmap.org")
+        print(type(result))
+        print(result)
         return 0
     
        
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from crawler.crawler import Crawler
+import timeit
+import asyncio
 
 def main():
+    start = timeit.default_timer()
     crawler = Crawler()
-    crawler.print_test()
+    crawler.scan_for_ports_opened('SMB')
+    stop = timeit.default_timer()
+    print ('Time :', stop - start, ' seconds')
 
 if __name__ == "__main__":
     main()
